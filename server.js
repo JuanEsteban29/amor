34cb2e9b1570e82709e04b83a93474b87cf01e0f
juan <juanbri2905@gmail.com>
2026-07-18 00:19:55 -0400

    Feat: versión inicial del proyecto para Kamila

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
+const fs = require('fs');
+console.log("Archivos en el directorio:", fs.readdirSync(__dirname));
 
 // Servir archivos estáticos desde la raíz
 app.use(express.static(path.resolve(__dirname)));
