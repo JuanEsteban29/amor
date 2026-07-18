e46025265616ba5c9ba12ee866465ff49d1ba765
juan <juanbri2905@gmail.com>
2026-07-17 23:53:37 -0400

    Feat: versión inicial del proyecto para Kamila

--- server.js
+++ server.js
@@ -4,10 +4,10 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(express.static(path.join(__dirname, '../')));
+app.use(express.static(path.resolve(__dirname, '..')));
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '../', 'index.html'));
+  res.sendFile(path.resolve(__dirname, '../', 'index.html'));
 });
 
 app.listen(PORT, () => {
