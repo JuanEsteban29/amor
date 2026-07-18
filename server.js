d292ca93da9845d7177c792fc4e4b91b37ccd437
juan <juanbri2905@gmail.com>
2026-07-18 00:26:56 -0400

    Feat: versión inicial del proyecto para Kamila

--- server.js
+++ server.js
@@ -2,18 +2,15 @@ const express = require('express');
 const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
-const fs = require('fs');
-console.log("Archivos en el directorio:", fs.readdirSync(__dirname));
 
-// Servir archivos estáticos desde la raíz
-app.use(express.static(path.resolve(__dirname)));
+// Servir los archivos estáticos desde la carpeta actual
+app.use(express.static(__dirname));
 
+// Asegurar la ruta absoluta al archivo index.html
 app.get('/', (req, res) => {
-    res.sendFile(path.join(process.cwd(), 'index.html'));
-    console.log("Intentando servir archivo desde: " + filePath); // Esto aparecerá en los Logs de Railway
-    res.sendFile(filePath);
+    res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 app.listen(PORT, () => {
-    console.log(`Servidor corriendo en el puerto ${PORT}`);
+    console.log(`Servidor iniciado en el puerto ${PORT}`);
 });
