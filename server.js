9b082a9922c4f50fbf5b652a8a3e53d5985a1fc5
juan <juanbri2905@gmail.com>
2026-07-18 01:29:33 -0400

    Feat: versión inicial del proyecto para Kamila

--- server.js
+++ server.js
@@ -1,15 +1,19 @@
 const express = require('express');
 const path = require('path');
-
 const app = express();
-const PORT = process.env.PORT || 3000;
 
-app.use(express.static(__dirname));
+// Usamos __dirname para asegurarnos de que el servidor siempre 
+// sepa dónde están los archivos, sin importar el sistema operativo.
+app.use(express.static(path.join(__dirname)));
 
+// Ruta para servir el archivo principal index.html
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'index.html'));
+    res.sendFile(path.join(__dirname, 'index.html'));
 });
 
+// Configuración del puerto, usa el que te proporcione el servicio en la nube 
+// o el 8080 por defecto.
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
-  console.log(`Servidor corriendo en el puerto ${PORT}`);
+    console.log(`Servidor corriendo en el puerto ${PORT}`);
 });
