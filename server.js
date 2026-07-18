1bae9c7d361bafeeb5207c5d14bf66b26959d33c
juan <juanbri2905@gmail.com>
2026-07-18 00:30:11 -0400

    Feat: versión inicial del proyecto para Kamila

--- server.js
+++ server.js
@@ -8,6 +8,8 @@ app.use(express.static(__dirname));
 
 // Asegurar la ruta absoluta al archivo index.html
 app.get('/', (req, res) => {
+    // __dirname es la carpeta donde vive server.js
+    // path.join combina la ruta de forma segura para cualquier sistema operativo
     res.sendFile(path.join(__dirname, 'index.html'));
 });
 
